78a5f1dcbc34500f01eca0c85c63073347f56e0f
HenryAnyiam <anyiamhenry7@gmail.com>
2025-11-05 13:54:16 +0100

    Tweak sidebar

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -1,12 +1,11 @@
 import { Routes, Route, Navigate } from "react-router-dom";
-import CandidateLayout from "../layout/CandidateLayout";
 import CandidateDashboard from "../layout/CandidateDashboard";
 import Dashboard from "../pages/Dashboard";
 
 function AppRoutes() {
   return (
     <Routes>
-      <Route path="/" element={<CandidateLayout />}>
+      <Route path="/" element={<CandidateDashboard />}>
       
         <Route index element={<CandidateDashboard />} />
         <Route path="Dashboard" element={<Dashboard />} />
